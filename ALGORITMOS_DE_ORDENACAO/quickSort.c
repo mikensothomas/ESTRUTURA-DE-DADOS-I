64210ea89f3d8af18ea80fbb833913eddf075d36
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-21 20:26:33 -0300

    Commit

--- ALGORITMOS_DE_ORDENACAO/quickSort.c
+++ ALGORITMOS_DE_ORDENACAO/quickSort.c
@@ -25,7 +25,7 @@ void quickSort(int *v, int inicio, int fim){
 }
 
 void imprimir(int *v, int tam){
-    for(int i = 0; i < tam; i++){
+    for(int i = 0; i <= tam; i++){
         printf("%d,",v[i]);
     }
     printf("\n");
@@ -33,7 +33,6 @@ void imprimir(int *v, int tam){
 
 int main(){
     int vet[] = {47,35,12,49,56,25,71,83,5,3,1,70,99,10,61};
-    int tam;
 
     imprimir(vet, 14);
 
